836bd1a138c5c785c8d65af691336e69dc946c4e
Belia Ramos <ramos.torres.belia@gmail.com>
2025-08-25 14:17:15 -0400

    Added last part of speed analysis exercise

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -43,5 +43,6 @@ function startTest() {
     outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" +
         "<p>Words Typed: " + typedWords + "</p>" +
         "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
-        "<p>Words Per Minute (WPM): " + wpm + "</p>";
+        "<p>Words Per Minute (WPM): " + wpm + "</p>" + 
+        "<p>Total length og the text: " + userTypedText.length + "</p>";
 }
